fad07aa288b3a4ed1002b8c3f998a615abd8c546
elamin <elaminhamid.elameen@elev.ga.ntig.se>
2025-10-06 11:15:36 +0200

    boo

---
+++ eleventy.config.js
@@ -0,0 +1,12 @@
+export default function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("img");
+
+    eleventyConfig.addShortcode("formatDate", () => {
+        return new Date().toLocaleDateString('sv-SE')
+    })
+
+    return {
+        markdownTemplateEngine: "njk",
+    }
+}
--- index.md
+++ index.md
@@ -1,1 +1,3 @@
-# hej 
+# Villo företag
+
+
